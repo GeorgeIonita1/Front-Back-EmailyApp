b0ec87436fd472d9419e4ce440ffae4e97f847f5
Mona Lisa <g3orrg301@gmail.com>
2019-11-22 17:25:09 +0200

    Laptop testing v2

---
+++ server/config/dev.js
@@ -0,0 +1,11 @@
+//dev.js
+module.exports = {
+    googleClientID: '281114915431-8mk1dto3mb7vl43urnbplhrand20pfls.apps.googleusercontent.com',
+    googleClientSecret: 'i2iFLoGCT-cTCto15rNm2piZ',
+    mongoURI: 'mongodb+srv://George:Prazoxelles4@emaily-dev-29uym.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'smthnjxcljg',
+    stripePublishableKey: 'pk_test_lTUPvsB5qCxmbPI5R2TrAdw200TLG6iYK6',
+    stripeSecretKey: 'sk_test_5WtMPiFmoH5xQFIjw9togu1h00vx9uiAXx',
+    sendGridKey: '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',
+    redirrectDomain: 'http://localhost:3000'
+};
